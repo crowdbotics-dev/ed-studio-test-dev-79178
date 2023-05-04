a8d2cabc7d812f3199854a2f7bad21fc9e01cac4
EduardoWeber <eduardo@crowdbotics.com>
2023-05-04 11:28:15 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,7 +1,8 @@
 export const globalOptions = {
-  name: "ed-studio-test-dev-79178",
-  url: "https://ed-studio-test-dev-79178.botics.co",
-  api: "https://ed-studio-test-dev-79178.botics.co/api/v1"
+  "name": "ed-studio-test-dev-79178",
+  "url": "https://ed-studio-test-dev-79178.botics.co",
+  "api": "https://ed-studio-test-dev-79178.botics.co/api/v1",
+  "initialRoute": "aboutTheApp"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
